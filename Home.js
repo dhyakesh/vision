f785cce9bf891273b83a4339f5f9c4c65a28e983
Dhyakesh Soundararajan <dhyakeshsoundarajan@gmail.com>
2018-12-31 19:32:21 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -15,12 +15,12 @@ import Tts from "react-native-tts";
 const firebase = require("firebase");
 //the config for the firebase
 var config = {
-  apiKey: "AIzaSyDO6tNkGcSGQVunHyNeEo-GAJDeNFoU2Vc",
-  authDomain: "vision-43124.firebaseapp.com",
-  databaseURL: "https://vision-43124.firebaseio.com",
-  projectId: "vision-43124",
-  storageBucket: "vision-43124.appspot.com",
-  messagingSenderId: "519317610341"
+  apiKey: "aaaaaaaaaaaaaaaaaaaaaa",
+  authDomain: "vision.firebaseapp.com",
+  databaseURL: "https://vision.firebaseio.com",
+  projectId: "vision",
+  storageBucket: "vision.appspot.com",
+  messagingSenderId: "55677827682618"
 };
 //initialize the firebase
 const app = firebase.initializeApp(config);
